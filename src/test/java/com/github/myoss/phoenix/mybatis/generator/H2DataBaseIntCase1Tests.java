2af61c17a6c8edba8050fe45258cf06d0f7e1425
Jerry.Chen <jerry.myoss@gmail.com>
2018-05-16 17:04:35 +0800

    feature: 为"序列生成器接口"，添加新的测试用例，使用 Sequence 实现类直接创建对象

--- src/test/java/com/github/myoss/phoenix/mybatis/generator/H2DataBaseIntCase1Tests.java
+++ src/test/java/com/github/myoss/phoenix/mybatis/generator/H2DataBaseIntCase1Tests.java
@@ -67,7 +67,7 @@ public class H2DataBaseIntCase1Tests {
 
     /**
      * 测试生成普通 Crud 文件。参考
-     * {@link com.github.myoss.phoenix.mybatis.test.integration.h2.test.UserControllerIntTests}
+     * {@link com.github.myoss.phoenix.mybatis.test.integration.h2.test1.UserControllerIntTests}
      * 集成测试类
      */
     @Test
@@ -90,7 +90,7 @@ public class H2DataBaseIntCase1Tests {
                 + "See the License for the specific language governing permissions and\n"
                 + "limitations under the License.\n");
         configuration.setRemoveEntityClassPrefix("TSys");
-        configuration.setRootPackageName("com.github.myoss.phoenix.mybatis.test.integration.h2");
+        configuration.setRootPackageName("com.github.myoss.phoenix.mybatis.test.integration.h2.test1");
         configuration.setRootOutputPath(rootOutputPath);
         configuration.setGenerateDate("2018年5月11日 上午10:41:47");
         configuration.addProperty(PropertyRegistry.ALL_METHOD_ENABLE_IN_WEB_FILE, true);
@@ -105,7 +105,7 @@ public class H2DataBaseIntCase1Tests {
 
     /**
      * 测试生成 {@link SelectKey} Crud 文件。参考
-     * {@link com.github.myoss.phoenix.mybatis.test.integration.h2.test.UserHistoryControllerIntTests}
+     * {@link com.github.myoss.phoenix.mybatis.test.integration.h2.test2.UserHistoryControllerIntTests}
      * 集成测试类
      */
     @Test
@@ -129,7 +129,7 @@ public class H2DataBaseIntCase1Tests {
                 + "limitations under the License.\n");
         configuration.setEntitySuperClass("com.github.myoss.phoenix.mybatis.repository.entity.AuditIdEntity");
         configuration.setRemoveEntityClassPrefix("TSys");
-        configuration.setRootPackageName("com.github.myoss.phoenix.mybatis.test.integration.h2");
+        configuration.setRootPackageName("com.github.myoss.phoenix.mybatis.test.integration.h2.test2");
         configuration.setRootOutputPath(rootOutputPath);
         configuration.setGenerateDate("2018年5月14日 下午3:39:43");
         configuration.addProperty(PropertyRegistry.ALL_METHOD_ENABLE_IN_WEB_FILE, true);
@@ -152,7 +152,7 @@ public class H2DataBaseIntCase1Tests {
 
     /**
      * 测试生成 {@link SequenceKey} Crud 文件。参考
-     * {@link com.github.myoss.phoenix.mybatis.test.integration.h2.test.UserLogControllerIntTests}
+     * {@link com.github.myoss.phoenix.mybatis.test.integration.h2.test3.UserLogControllerIntTests}
      * 集成测试类
      */
     @Test
@@ -176,7 +176,7 @@ public class H2DataBaseIntCase1Tests {
                 + "limitations under the License.\n");
         configuration.setEntitySuperClass("com.github.myoss.phoenix.mybatis.repository.entity.AuditIdEntity");
         configuration.setRemoveEntityClassPrefix("TSys");
-        configuration.setRootPackageName("com.github.myoss.phoenix.mybatis.test.integration.h2");
+        configuration.setRootPackageName("com.github.myoss.phoenix.mybatis.test.integration.h2.test3");
         configuration.setRootOutputPath(rootOutputPath);
         configuration.setGenerateDate("2018年5月14日 下午10:33:55");
         configuration.addProperty(PropertyRegistry.ALL_METHOD_ENABLE_IN_WEB_FILE, false);
@@ -190,6 +190,55 @@ public class H2DataBaseIntCase1Tests {
         configuration.setSequenceStrategy(GenerationType.SEQUENCE_KEY);
         configuration
                 .addEntityImportPackage("com.github.myoss.phoenix.mybatis.test.integration.h2.H2DataBaseIntTest.SequenceCustomizer");
+        //        configuration.setSequenceTemplatePath("*/templates/freemarker/copyright.ftl");
+
+        TableConfiguration tableConfiguration = new TableConfiguration();
+        tableConfiguration.setTableName("t_sys_user_log");
+        tableConfigurations.add(tableConfiguration);
+
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(configuration);
+        myBatisGenerator.generate();
+    }
+
+    /**
+     * 测试生成 {@link SequenceKey} Crud 文件。参考
+     * {@link com.github.myoss.phoenix.mybatis.test.integration.h2.test4.UserLogControllerIntTests}
+     * 集成测试类
+     */
+    @Test
+    public void generateTest4() {
+        String rootOutputPath = getRootOutputPath("generateTest4", false);
+
+        List<TableConfiguration> tableConfigurations = new ArrayList<>();
+        Configuration configuration = new Configuration();
+        configuration.setDataSource(dataSource);
+        configuration.setTableConfigurations(tableConfigurations);
+        configuration.setAuthor("jerry");
+        configuration.setCopyright("Copyright 2018-${todayYear} https://github.com/myoss\n" + "\n"
+                + "Licensed under the Apache License, Version 2.0 (the \"License\");\n"
+                + "you may not use this file except in compliance with the License.\n"
+                + "You may obtain a copy of the License at\n" + "\n"
+                + "    http://www.apache.org/licenses/LICENSE-2.0\n" + "\n"
+                + "Unless required by applicable law or agreed to in writing, software\n"
+                + "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+                + "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+                + "See the License for the specific language governing permissions and\n"
+                + "limitations under the License.\n");
+        configuration.setEntitySuperClass("com.github.myoss.phoenix.mybatis.repository.entity.AuditIdEntity");
+        configuration.setRemoveEntityClassPrefix("T");
+        configuration.setRootPackageName("com.github.myoss.phoenix.mybatis.test.integration.h2.test4");
+        configuration.setRootOutputPath(rootOutputPath);
+        configuration.setGenerateDate("2018年5月14日 下午10:33:55");
+        configuration.addProperty(PropertyRegistry.ALL_METHOD_ENABLE_IN_WEB_FILE, false);
+        configuration.addProperty(PropertyRegistry.SEQUENCE_KEY_SEQUENCE_CLASS, "SequenceCustomizer");
+        configuration.addProperty(PropertyRegistry.SEQUENCE_KEY_SEQUENCE_NAME, "sequenceUserLog");
+        configuration.addProperty(PropertyRegistry.SEQUENCE_KEY_ORDER,
+                Order.class.getSimpleName() + "." + Order.BEFORE.name());
+        configuration.addProperty(PropertyRegistry.USE_PRIMARY_KEY_JAVA_TYPE_FOR_CLASS_GENERIC_TYPE_IN_ENTITY_FILE,
+                true);
+        configuration.setSequenceStrategy(GenerationType.SEQUENCE_KEY);
+        configuration
+                .addEntityImportPackage("com.github.myoss.phoenix.mybatis.test.integration.h2.test4.SysUserLogControllerIntTests.SequenceCustomizer");
         //        configuration.setSequenceTemplatePath("*/templates/freemarker/copyright.ftl");
 
         TableConfiguration tableConfiguration = new TableConfiguration();
